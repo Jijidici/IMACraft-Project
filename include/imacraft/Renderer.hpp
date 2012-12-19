a992f5daec09b0fa9f21719c6e3dc821af8cd00e
CedricBidaud <cb1989@hotmail.fr>
2012-12-19 09:55:59 +0100

    add Renderer structure

---
+++ include/imacraft/Renderer.hpp
@@ -0,0 +1,19 @@
+#ifndef _RENDERER_HPP_
+#define _RENDERER_HPP_
+
+//~ #include "imacraft/CubeInstance.hpp"
+//~ #include "imacraft/TerrainGrid.hpp"
+
+namespace imacraft{
+	struct Renderer{
+		//~ CubeInstance m_cubeModel;
+		//~ TerrainGrid m_grid;
+		//~ Renderer(CubeInstance cubeModel, TerrainGrid grid);
+		
+		void render();
+		
+		~Renderer();
+	};
+}
+
+#endif // _RENDERER_HPP_
