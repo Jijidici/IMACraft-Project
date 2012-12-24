9caa18957735dae446fb49230c454218a9f1d799
Jijidici <jdefaye.jijidici@gmail.com>
2012-12-24 15:30:00 +0100

    Reset fragment shaders to compute no illumination

--- shaders/normalcolor.fs.glsl
+++ shaders/normalcolor.fs.glsl
@@ -5,22 +5,7 @@ in vec2 vTexCoords;
 
 out vec4 fFragColor;
 
-float dotProduct(vec3 v1, vec3 v2){
-	return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
-}
-
-float norm(vec3 v){
-	return sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
-}
-
-vec3 normalize(vec3 v){
-	float vec_norm = norm(v);
-	return vec3(v.x/vec_norm, v.y/vec_norm, v.z/vec_norm);
-}
-
 void main() {
-	vec3 light = vec3(1.f, -1.f, 0.f);
-	float simpleLightProcess = dotProduct(normalize(vNormal), normalize(light));
-	fFragColor = vec4(0.7f - simpleLightProcess, 0.8f - simpleLightProcess, 1.f - simpleLightProcess, 1.f);
+	fFragColor = vec4(0.58f, 0.44f, 0.07f, 1.f);
 }
 
